fe473caf86740198f5f9b2ca83ccaa8eea67f884
halajda <halajdabenj@gmail.com>
2024-01-02 10:19:18 +0100

    optimisation du backend

--- backend/app.js
+++ backend/app.js
@@ -1,110 +1,23 @@
+const express = require('express');
+
+const path = require('path');
+
+
 const mongoose = require('mongoose');
+
+const stuffRoutes = require('./routes/stuff');
+
 mongoose.connect('mongodb+srv://benjamin:coco14@cluster0-pme76.mongodb.net/test?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
+const app = express();
+app.use(express.json());
 
-  const express = require('express');
-  const stuffRoutes = require('./routes/stuff');
-  app.use('/api/stuff', stuffRoutes);
-  const router = express.Router();
-  
-  const Thing = require('../models/thing');
-  
-  router.post('/', (req, res, next) => {
-    const thing = new Thing({
-      title: req.body.title,
-      description: req.body.description,
-      imageUrl: req.body.imageUrl,
-      price: req.body.price,
-      userId: req.body.userId
-    });
-    thing.save().then(
-      () => {
-        res.status(201).json({
-          message: 'Post saved successfully!'
-        });
-      }
-    ).catch(
-      (error) => {
-        res.status(400).json({
-          error: error
-        });
-      }
-    );
-  });
-  
-  router.get('/:id', (req, res, next) => {
-    Thing.findOne({
-      _id: req.params.id
-    }).then(
-      (thing) => {
-        res.status(200).json(thing);
-      }
-    ).catch(
-      (error) => {
-        res.status(404).json({
-          error: error
-        });
-      }
-    );
-  });
-  
-  router.put('/:id', (req, res, next) => {
-    const thing = new Thing({
-      _id: req.params.id,
-      title: req.body.title,
-      description: req.body.description,
-      imageUrl: req.body.imageUrl,
-      price: req.body.price,
-      userId: req.body.userId
-    });
-    Thing.updateOne({_id: req.params.id}, thing).then(
-      () => {
-        res.status(201).json({
-          message: 'Thing updated successfully!'
-        });
-      }
-    ).catch(
-      (error) => {
-        res.status(400).json({
-          error: error
-        });
-      }
-    );
-  });
-  
-  router.delete('/:id', (req, res, next) => {
-    Thing.deleteOne({_id: req.params.id}).then(
-      () => {
-        res.status(200).json({
-          message: 'Deleted!'
-        });
-      }
-    ).catch(
-      (error) => {
-        res.status(400).json({
-          error: error
-        });
-      }
-    );
-  });
-  
-  router.get('/' +
-    '', (req, res, next) => {
-    Thing.find().then(
-      (things) => {
-        res.status(200).json(things);
-      }
-    ).catch(
-      (error) => {
-        res.status(400).json({
-          error: error
-        });
-      }
-    );
-  });
-  
-  module.exports = router;
+app.use('/api/stuff', stuffRoutes);
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+module.exports = app;
